33d50e6edadcdbd330a80befdd1c92d24ad24ced
Matthew Liddle <matthew.liddle.2021@uni.strath.ac.uk>
2023-02-22 19:40:26 +0000

    Implemented fetching from endpoint

--- src/main/java/uk/co/asepstrath/bank/BankController.java
+++ src/main/java/uk/co/asepstrath/bank/BankController.java
@@ -1,23 +1,14 @@
 package uk.co.asepstrath.bank;
 
-import com.google.gson.Gson;
 import io.jooby.ModelAndView;
-import io.jooby.StatusCode;
 import io.jooby.annotations.*;
-import io.jooby.exception.StatusCodeException;
+import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.slf4j.Logger;
 
 import javax.sql.DataSource;
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 @Path("/")
 public class BankController {
@@ -34,37 +25,14 @@ public class BankController {
     }
 
     private ArrayList<Account> getAccounts() {
-        try (Connection connection = dataSource.getConnection()) {
-            // Create Statement (batch of SQL Commands)
-            Statement statement = connection.createStatement();
-            // Perform SQL Query
-            ResultSet set = statement.executeQuery("SELECT * FROM `userAccounts`");
-
-            ArrayList<Account> Accounts = new ArrayList<Account>();
-            while(set.next()) {
-                Account a = new Account(set.getString("Name"), set.getBigDecimal("Balance"));
-                Accounts.add(a);
-            }
-            // Return value
-            return Accounts;
-        } catch (SQLException e) {
-            // If something does go wrong this will log the stack trace
-            logger.error("Database Error Occurred",e);
-            // And return a HTTP 500 error to the requester
-            throw new StatusCodeException(StatusCode.SERVER_ERROR, "Database Error Occurred");
-        }
-    }
-
-    @GET("/api")
-    public String GET_api() {
-        return new Gson().toJson(getAccounts());
+        HttpResponse<ArrayList> res = Unirest.get("http://api.asep-strath.co.uk/api/Team8/accounts").asObject(ArrayList.class);
+        return (ArrayList<Account>) res.getBody();
     }
 
     @GET
     public ModelAndView viewAccounts() {
-        HashMap hm = new HashMap<String,Object>();
-        String json = GET_api();
-        ArrayList<Account> accs = new Gson().fromJson(json, ArrayList.class);
+        HashMap<String, Object> hm = new HashMap<>();
+        ArrayList<Account> accs = getAccounts();
         hm.put("accounts",accs);
         return new ModelAndView("accounts.hbs", hm);
     }
--- src/test/java/uk/co/asepstrath/bank/BankControllerTests.java
+++ src/test/java/uk/co/asepstrath/bank/BankControllerTests.java
@@ -33,31 +33,4 @@ public class BankControllerTests {
         Assertions.assertNotNull(bankController);
     }
 
-    @Test
-    @DisplayName("BankController GET_api()")
-    public void getAccounts() throws SQLException {
-        BankController bankController = new BankController(ds, log);
-
-        Connection con = mock(Connection.class);
-        when(ds.getConnection()).thenReturn(con);
-        Statement stmt = mock(Statement.class);
-        when(con.createStatement()).thenReturn(stmt);
-        ResultSet set = mock(ResultSet.class);
-        when(stmt.executeQuery("SELECT * FROM `userAccounts`")).thenReturn(set);
-
-        when(set.next()).thenReturn(true).thenReturn(true).thenReturn(false);
-        when(set.getString("Name")).thenReturn("John Smith").thenReturn("Joe Bloggs");
-        when(set.getBigDecimal("Balance")).thenReturn(new BigDecimal(10)).thenReturn(new BigDecimal(25));
-
-        String json = bankController.GET_api();
-        Assertions.assertEquals("[{\"name\":\"John Smith\",\"balance\":10},{\"name\":\"Joe Bloggs\",\"balance\":25}]", json);
-    }
-
-    @Test
-    @DisplayName("BankController GET_api() SQLException")
-    public void getAccountsSQLException() throws SQLException {
-        BankController bankController = new BankController(ds, log);
-        when(ds.getConnection()).thenThrow(new SQLException());
-        Assertions.assertThrows(StatusCodeException.class, bankController::GET_api);
-    }
 }
