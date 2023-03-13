fe321f7ce7e67dcf404b7b30dd1c5b57674b28c2
Matthew Liddle SE2021 <matthew.liddle.2021@uni.strath.ac.uk>
2023-03-13 19:44:23 +0000

    Implement API tests

--- src/test/java/uk/co/asepstrath/bank/BankExportControllerTests.java
+++ src/test/java/uk/co/asepstrath/bank/BankExportControllerTests.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankExportControllerTests {
+class BankExportControllerTests {
 
     static BankExportController bankExportController;
 
